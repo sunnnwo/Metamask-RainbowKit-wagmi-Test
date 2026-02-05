8cbaf42d42a76570115e608bee529349529dbd6d
sunwoo <sun-wkim@student.42lausanne.ch>
2026-02-05 15:44:47 +0100

    Allowance & approve

--- src/pages/hooks.tsx
+++ src/pages/hooks.tsx
@@ -1,4 +1,4 @@
-import { useAccount, useBalance, useReadContract, useReadContracts } from 'wagmi'
+import { useAccount, useBalance, useReadContract, useReadContracts, useWriteContract  } from 'wagmi'
 import { erc20Abi } from 'viem';
 import { useChainId } from 'wagmi'
 
@@ -7,9 +7,11 @@ import { useChainId } from 'wagmi'
 export function Hooks() {
     const account = useAccount();
     const chainId = useChainId();
+    const writeContract = useWriteContract()
+
 
     console.log("chainId: ", chainId);
-    const TokenAddress: `0x${string}` = chainId === 1 
+    const TokenAddress: `0x${string}` = chainId === 1 //반드시 0x로 시작해야함
         ? "0xdAC17F958D2ee523a2206206994597C13D831ec7" 
         : "0xaA8E23Fb1079EA71e0a56F48a2aA51851D8433D0"; // Sepolia
 
@@ -31,10 +33,17 @@ export function Hooks() {
             // address: TokenAddress,
             ...wagmiReadContract,
             functionName: 'symbol',
+        },
+        {
+            // abi: erc20Abi,
+            // address: TokenAddress,
+            ...wagmiReadContract,
+            args: [account.address as `0x${string}`,  "0x1f79BD178EcFbF880903E31C45206670704043AC" as `0x${string}`],
+            functionName: 'allowance',
         }
     
         ],})
-   
+         
     console.log("result: ", result);
     const {data: balance, refetch} = useBalance({
     address: account.address,
@@ -46,6 +55,16 @@ export function Hooks() {
             <li>ChainId : {chainId}</li>
             <li>Total Supply:  {result.data?.[0]?.result?.toString()}</li>
             <li>Symbol:  {result.data?.[1]?.result?.toString()}</li>
+            <li>Allowance:  {result.data?.[2]?.result?.toString()}</li>
+            <button onClick={()=>{
+                writeContract.writeContract({
+                    abi: erc20Abi,
+                    address: TokenAddress,
+                    functionName: 'approve',
+                    args: ["0x1f79BD178EcFbF880903E31C45206670704043AC" as `0x${string}`, BigInt(100000000000000000000n)],
+                })
+            }}
+            >Approve</button>
         </ul>
     </div>
   </>
