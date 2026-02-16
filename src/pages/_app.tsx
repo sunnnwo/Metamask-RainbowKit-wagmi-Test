330574870777f2a04e1bb2df47240b7c325c7111
sunwoo <sun-wkim@student.42lausanne.ch>
2026-02-16 13:57:11 +0100

    Build: successfully

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,6 @@
 import '../styles/globals.css';
 import '@rainbow-me/rainbowkit/styles.css';
-import './index.css';
+import  '/home/sunwoo/sunwoo/Test_rainbow/my-rainbowkit-app/src/styles/index.css'
 import type { AppProps } from 'next/app';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
